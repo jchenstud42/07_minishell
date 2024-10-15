07e1f0104b6adecfeefe580127ed23ced2d2f677
Romain Bouquet <rbouquet@made-f0Dr10s17.clusters.42paris.fr>
2024-10-15 14:23:29 +0200

    exec

---
+++ src/exec/exec.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbouquet <rbouquet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 14:09:49 by rbouquet          #+#    #+#             */
+/*   Updated: 2024/10/15 14:22:07 by rbouquet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	is_builtins(char *cmd)
+{
+	if (!cmd)
+		return (1);
+	if (!ft_strcmp("echo", cmd) || !ft_strcmp("cd", cmd) || \
+		!ft_strcmp("pwd", cmd) || !ft_strcmp("export", cmd) || \
+		!ft_strcmp("unset", cmd) || !ft_strcmp("env", cmd) || \
+		!ft_strcmp("exit", cmd))
+		return (1);
+	return (0);
+}
+
