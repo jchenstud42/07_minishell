571e715261e77a90db3a71fb0bb3d18ec31217a7
Romain Bouquet <rbouquet@made-f0Br5s12.clusters.42paris.fr>
2024-10-21 09:55:04 +0200

    pwd

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: rbouquet <rbouquet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 10:07:37 by rbouquet          #+#    #+#             */
-/*   Updated: 2024/10/15 15:14:40 by rbouquet         ###   ########.fr       */
+/*   Updated: 2024/10/21 09:35:53 by rbouquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,18 @@ void	print_echo(int nbr_caract, char **av)
 	{
 		write(1, av[i], ft_strlen(av[i]));
 		if (i != nbr_caract - 1)
-			write(1, ' ', 1);
+			write(1, " ", 1);
 		i++;
 	}
 	if (add_line == 0)
-		write(1, '\n', 1);
+		write(1, "\n", 1);
 }
 
 void	ft_echo(char **av)
 {
 	int	nbr_caract;
 
+	nbr_caract = 0;
 	while (av[nbr_caract])
 		nbr_caract++;
 	print_echo(nbr_caract, av);
@@ -51,8 +52,9 @@ int	check_n(char *line)
 	i = 0;
 	if (!line)
 		return (1);
-	if (line[i] || line[i] == '-')
+	if (line[i] && line[i] == '-')
 	{
+		i++;
 		while (line[i] && line[i] == 'n')
 			i++;
 		if (i == ft_strlen(line))
--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,9 +6,25 @@
 /*   By: rbouquet <rbouquet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 11:32:20 by rbouquet          #+#    #+#             */
-/*   Updated: 2024/10/15 11:32:52 by rbouquet         ###   ########.fr       */
+/*   Updated: 2024/10/21 09:40:17 by rbouquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	ft_pwd(void)
+{
+	char	*pwd;
+
+	pwd = getcwd(NULL, 0);
+	if (pwd != NULL)
+		printf("%s\n", pwd);
+	else
+	{
+		printf("error\n");
+		free(pwd);
+		return (1);
+	}
+	free(pwd);
+	return (0);
+}
