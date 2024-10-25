ee569c7ca58a5a27ef314bcca31a64e45c129234
Romain Bouquet <rbouquet@made-f0Br4s12.clusters.42paris.fr>
2024-10-25 12:49:49 +0200

    je go miam miam

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -5,14 +5,10 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rbouquet <rbouquet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/15 11:31:41 by rbouquet          #+#    #+#             */
-/*   Updated: 2024/10/25 11:41:19 by rbouquet         ###   ########.fr       */
+/*   Created: 2024/10/25 12:34:39 by rbouquet          #+#    #+#             */
+/*   Updated: 2024/10/25 12:43:07 by rbouquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-// int	ft_cd(t_global *data, char **params)
-// {
-
-// }
