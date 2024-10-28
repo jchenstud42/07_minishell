75891cc80b5524116dacc1b7a5eae88122bb52e1
Romain Bouquet <rbouquet@made-f0Ar5s2.clusters.42paris.fr>
2024-10-28 12:28:04 +0100

    Commencement export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,9 +6,32 @@
 /*   By: rbouquet <rbouquet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 11:32:12 by rbouquet          #+#    #+#             */
-/*   Updated: 2024/10/28 11:17:33 by rbouquet         ###   ########.fr       */
+/*   Updated: 2024/10/28 11:53:28 by rbouquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
+void	export_no_arg(t_env *env)
+{
+	char	env_tab;
+	int		i;
+	int		j;
+
+	env_tab = get_env(env); //FONCTION A FINIR
+	if (!env_tab)
+		return (0);
+	sort_env(env_tab); //FONCTION A CREER
+	i = 0;
+	while (env_tab[i])
+	{
+		j = 0;
+		ft_printf("declare -x ");
+		while (env_tab[i][j] && env[i][j] != '\n')
+		{
+			ft_printf("%s", env_tab[i][j])
+			j++;
+		}
+		if ()
+	}
+}
