e4c4ea6f01959ddccf40f511142a38f45c1f36f6
Romain Bouquet <rbouquet@made-f0Br8s18.clusters.42paris.fr>
2024-11-15 15:26:27 +0100

    J'essaie de toujours regler ce truc feiw vbab gds

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: rbouquet <rbouquet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 09:44:15 by rbouquet          #+#    #+#             */
-/*   Updated: 2024/11/15 14:08:57 by rbouquet         ###   ########.fr       */
+/*   Updated: 2024/11/15 15:26:03 by rbouquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,49 +26,4 @@ int	unset_syntaxe(char *line)
 		i++;
 	}
 	return (0);
-}
-
-int	unset(t_env **env, char *av)
-{
-	int		index;
-	int		i;
-	t_env	*tmp;
-
-	if (!av || !*av)
-		return (1);
-	if (!unset_syntaxe(av))
-		return (1);
-	index = check_env_line_exist(*env, av);
-	if (index == -1)
-		return (1);
-	tmp = *env;
-	i = 0;
-	while (i++ < index)
-		tmp = tmp->next;
-	if (tmp->prev != NULL)
-		tmp->prev->next = tmp->next;
-	if (tmp->next != NULL)
-		tmp->next->prev = tmp->prev;
-	if (tmp == *env)
-		*env = tmp->next;
-	free(tmp->env);
-	free(tmp);
-	tmp = NULL;
-	return (1);
-}
-
-int	ft_unset(t_env **env, char **args)
-{
-	int	exit_code;
-	int	i;
-
-	exit_code = 0;
-	i = 0;
-	while (args[i])
-	{
-		if (unset(env, args[i]))
-			exit_code = 1;
-		i++;
-	}
-	return (exit_code);
 }
